3a85a729766f7a37b975a1208afd700398a4d1bb
Kevin <nivek325@gmail.com>
2016-04-06 20:04:12 -0700

    Use the Yelp API to generate businesses in Isla Vista

--- app/src/main/java/com/example/user01/planit/MainActivity.java
+++ app/src/main/java/com/example/user01/planit/MainActivity.java
@@ -8,8 +8,79 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.Business;
+import com.yelp.clientlib.entities.SearchResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+    int randomInt;
+    ArrayList<Business> businesses = new ArrayList<Business>();
+
+    public void getStuff() {
+        randomInt = randInt(0,20);
+        YelpAPIFactory yelpAPIFactory = new YelpAPIFactory(
+                getString(R.string.consumer_key),
+                getString(R.string.consumer_secret),
+                getString(R.string.token),
+                getString(R.string.token_secret));
+        YelpAPI yelpAPI = yelpAPIFactory.createAPI();
+
+        Map<String, String> params = new HashMap<>();
+        params.put("radius_filter", "16000");
+        Call<SearchResponse> call = yelpAPI.search("Goleta", params);
+
+        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Response<SearchResponse> response, Retrofit retrofit) {
+                SearchResponse searchResponse = response.body();
+                businesses = searchResponse.businesses();
+                randomInt = randInt(0,businesses.size()-1);
+                TextView businessTextView = (TextView) findViewById(R.id.BusinessName);
+                businessTextView.setText(businesses.get(randomInt).name());
+
+                TextView addressTextView = (TextView) findViewById(R.id.Address);
+                addressTextView.setText(businesses.get(randomInt).location().address().get(0));
+
+                TextView snippetTextView = (TextView) findViewById(R.id.SnippetText);
+                snippetTextView.setText(businesses.get(randomInt).snippetText());
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        };
+        call.enqueue(callback);
+    }
+
+    public void buttonPress(View view) {
+        getStuff();
+    }
+
+    public static int randInt(int min, int max) {
+
+        // Usually this can be a field rather than a method variable
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
