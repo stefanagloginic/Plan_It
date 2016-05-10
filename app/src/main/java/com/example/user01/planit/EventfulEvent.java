b52db36e5284b8f147764abf723ef1d20b7f01fa
jimmy le <lejimmyl@yahoo.com>
2016-05-09 20:06:48 -0700

    Add classes for Eventful API

---
+++ app/src/main/java/com/example/user01/planit/EventfulEvent.java
@@ -0,0 +1,29 @@
+package com.example.user01.planit;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class EventfulEvent extends Event {
+
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("city_name")
+    @Expose
+    private String cityName;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("venue_address")
+    @Expose
+    private String venueAddress;
+
+    public void setEventVariables() {
+        this.eventName = title;
+        this.eventAddress = venueAddress + " " + cityName;
+        this.eventRating = "N/A";
+        this.eventURL = url;
+        this.eventPriceRange = "N/A";
+    }
+
+}
---
+++ app/src/main/java/com/example/user01/planit/EventfulModel.java
@@ -0,0 +1,44 @@
+
+
+package com.example.user01.planit;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class EventfulModel {
+
+    @SerializedName("last_item")
+    @Expose
+    private Object lastItem;
+    @SerializedName("total_items")
+    @Expose
+    private String totalItems;
+    @SerializedName("first_item")
+    @Expose
+    private Object firstItem;
+    @SerializedName("page_number")
+    @Expose
+    private String pageNumber;
+    @SerializedName("page_size")
+    @Expose
+    private String pageSize;
+    @SerializedName("page_items")
+    @Expose
+    private Object pageItems;
+    @SerializedName("search_time")
+    @Expose
+    private String searchTime;
+    @SerializedName("page_count")
+    @Expose
+    private String pageCount;
+    @SerializedName("events")
+    @Expose
+    private Events events;
+
+    public Events getEvents() {
+        return events;
+    }
+
+}
+
+
