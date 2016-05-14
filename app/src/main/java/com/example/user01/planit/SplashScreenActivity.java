ff6baeaedfb1fcdbee37a97e628091f296f8ad28
jimmy le <lejimmyl@yahoo.com>
2016-05-13 21:16:41 -0700

    Update SplashScreen

--- app/src/main/java/com/example/user01/planit/SplashScreenActivity.java
+++ app/src/main/java/com/example/user01/planit/SplashScreenActivity.java
@@ -4,17 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         this.finish();
+
     }
 
 
